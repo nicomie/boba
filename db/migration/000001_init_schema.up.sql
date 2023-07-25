b37dc021b5e2728acb9c91b1cf81f50eaaff4125
nicomie <editsbymystic@gmail.com>
2023-07-25 21:40:42 +0200

    removed currency

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY,
   "name" varchar NOT NULL,
   "balance" bigint NOT NULL,
-  "currency" varchar NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
