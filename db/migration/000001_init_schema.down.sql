fba1febd64ac4c34601ad3f0c766c558ead85d93
nicomie <editsbymystic@gmail.com>
2023-07-26 14:04:10 +0200

    schema fixes

--- db/migration/000001_init_schema.down.sql
+++ db/migration/000001_init_schema.down.sql
@@ -1,6 +1,8 @@
-DROP TABLE "transfers";
-DROP TABLE "entries";
-DROP TABLE "order_details";
-DROP TABLE "orders";
-DROP TABLE "items";
-DROP TABLE "accounts";
+DROP TABLE IF EXISTS "order_items";
+DROP TABLE IF EXISTS "transfers";
+DROP TABLE IF EXISTS "entries";
+DROP TABLE IF EXISTS "orders";
+DROP TABLE IF EXISTS "accounts";
+DROP TABLE IF EXISTS "products";
+
+
--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -5,56 +5,60 @@ CREATE TABLE "accounts" (
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE "items" (
-  "item_id" bigserial PRIMARY KEY,
-  "item_name" varchar NOT NULL,
-  "amount" bigint NOT NULL
-);
-
-CREATE TABLE "order_details" (
-  "order_detail_id" bigserial PRIMARY KEY,
-  "order_id" bigserial,
-  "item_id" bigserial,
-  "quantity" int NOT NULL
-);
-
-CREATE TABLE "orders" (
-  "id" bigserial PRIMARY KEY
-);
-
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
-  "order_id" bigserial,
-  "account_id" bigint,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
-  "from_account_id" bigint,
-  "to_account_id" bigint,
-  "amount" bigint NOT NULL,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "order_id" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "products" (
+  "id" bigserial PRIMARY KEY,
+  "name" varchar UNIQUE NOT NULL,
+  "price" int NOT NULL
+);
+
+CREATE TABLE "orders" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint UNIQUE NOT NULL,
+  "status" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "order_items" (
+  "order_id" bigint NOT NULL,
+  "product_id" bigint NOT NULL,
+  "quantity" int DEFAULT 1
 );
 
 CREATE INDEX ON "accounts" ("name");
 
-CREATE INDEX ON "entries" ("id");
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
 
 CREATE INDEX ON "transfers" ("to_account_id");
 
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-COMMENT ON COLUMN "transfers"."amount" IS 'it must be positive';
-
-ALTER TABLE "order_details" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
-
-ALTER TABLE "order_details" ADD FOREIGN KEY ("item_id") REFERENCES "items" ("item_id");
-
-ALTER TABLE "entries" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+COMMENT ON COLUMN "entries"."amount" IS 'Can be negative or positive';
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
 ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "orders" ("id");
+
+ALTER TABLE "order_items" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
