13bd79e4388237401d39015c29f547b8c1c4cc9a
nicomie <editsbymystic@gmail.com>
2023-08-16 14:42:25 +0200

    hash

---
+++ util/pin.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPin(pin string) (string, error) {
+
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pin), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", fmt.Errorf("failed to hash pin: %w", err)
+	}
+
+	return string(hashed), nil
+
+}
