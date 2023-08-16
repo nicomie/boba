efd1c3ada71d195dfae1c9880c5b3add8bce183a
nicomie <editsbymystic@gmail.com>
2023-08-16 15:47:02 +0200

    trigger test

--- db/migration/000002_add_boba_shops.down.sql
+++ db/migration/000002_add_boba_shops.down.sql
@@ -5,7 +5,6 @@ ALTER TABLE "orders" DROP CONSTRAINT "orders_boba_shop_fkey";
 
 ALTER TABLE "orders" DROP COLUMN "boba_shop";
 
-
 DROP TABLE IF EXISTS "personnel";
 
 DROP TABLE IF EXISTS "boba_shop";
