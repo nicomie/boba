a56c3f8846c2a341f2ac7bf7a3d62d968fc46842
nicomie <editsbymystic@gmail.com>
2023-08-25 16:27:13 +0200

    auth middleware

---
+++ api/middleware.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"boba/token"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	authHeaderKey  = "auth"
+	authTypeBearer = "bearer"
+	authPayloadKey = "auth_payload"
+)
+
+func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader(authHeaderKey)
+
+		if len(authHeader) == 0 {
+			err := errors.New("auth header is not provided")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		fields := strings.Fields(authHeader)
+		if len(fields) < 2 {
+			err := errors.New("invalid auth header format")
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		authType := strings.ToLower(fields[0])
+		if authType != authTypeBearer {
+			err := fmt.Errorf("unsupported auth type %s", authType)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		accessToken := fields[1]
+
+		payload, err := tokenMaker.VerifyToken(accessToken)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, errorResponse(err))
+			return
+		}
+
+		ctx.Set(authPayloadKey, payload)
+		ctx.Next()
+
+	}
+
+}
